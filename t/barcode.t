5e69f84d479752371c1272d89257a2672fed5834
Manuel Rueda <rueda.manu@gmail.com>
2023-12-16 11:48:34 +0100

    Update barcode

--- t/barcode.t
+++ t/barcode.t
@@ -2,7 +2,7 @@
 use strict;
 use warnings;
 use File::Spec::Functions qw(catdir catfile);
-use Test::More tests => 3;    # Indicate the number of tests you want to run
+use Test::More tests => 5;    # Indicate the number of tests you want to run
 use File::Compare;
 use List::MoreUtils qw(pairwise);
 
@@ -78,7 +78,7 @@ use List::MoreUtils qw(pairwise);
     my $script = catfile( 'utils', 'barcode', 'pheno-ranker2pdf' );
 
     # Input file for the command line script, if needed
-    my $qr   = catfile( 't',    'qr_codes', '107_week_0_arm_1.png' );
+    my $qr   = catfile( 't',    'qr_codes', '107_week_0_arm_1.compressed.png' );
     my $logo = catfile( 'docs', 'img',      'PR-logo.png' );
     my $json = catfile( 't',    'qr_codes', 'output.json' );
 
@@ -106,32 +106,62 @@ use List::MoreUtils qw(pairwise);
 # TEST 4 #
 ##########
 
-#{
-##
-#    # The command line script to be tested
-#    my $script = catfile( 'utils', 'barcode', 'pheno-ranker2barcode' );
-#
-#    # Input file for the command line script, if needed
-#    my $input_file = catfile( 't', 'export.ref_binary_hash.json' );
-#
-#    # The reference files to compare the output with
-#    my $reference_file = catfile( 't', 'qr_codes', '107_week_0_arm_1.compressed.png' );
-#
-#    # The output files
-#    my $output_dir  = catdir( 't', 'tmp_test' );
-#    my $output_file = catfile( $output_dir, '107_week_0_arm_1.compressed.png' );
-#
-#    # Run the command line
-#    system("$script -i $input_file -o $output_dir");
-#
-#    # Compare the output_file and the reference_file
-#    ok(
-#        compare( $output_file, $reference_file ) == 0,
-#        qq/Output matches the <$reference_file> file/
-#    );
-#    unlink $output_file;
-#    rmdir $output_dir;
-#}
+{
+    # The command line script to be tested
+    my $script = catfile( 'utils', 'barcode', 'pheno-ranker2barcode' );
+
+    # Input file for the command line script, if needed
+    my $input_file = catfile( 't', 'export.ref_binary_hash.json' );
+
+    # The reference files to compare the output with
+    my $reference_file = catfile( 't', 'qr_codes', '107_week_0_arm_1.compressed.png' );
+
+    # The output files
+    my $output_dir  = catdir( 't', 'tmp_test' );
+    my $output_file = catfile( $output_dir, '107_week_0_arm_1.png' );
+
+    # Run the command line
+    system("$script -i $input_file -o $output_dir");
+
+    # Compare the output_file and the reference_file
+    ok(
+        compare( $output_file, $reference_file ) == 0,
+        qq/Output matches the <$reference_file> file/
+    );
+    unlink $output_file;
+    rmdir $output_dir;
+}
+
+##########
+# TEST 5 #
+##########
+
+{
+
+    # The command line script to be tested
+    my $script = catfile( 'utils', 'barcode', 'barcode2pheno-ranker' );
+
+    # Input file for the command line script, if needed
+    my $input_file    = catfile( 't', 'qr_codes', '107_week_0_arm_1.compressed.png' );
+    my $template_file = catfile( 't', 'export.glob_hash.json' );
+
+    # The reference files to compare the output with
+    my $reference_file = catfile( 't', 'qr_codes', 'output.json' );
+
+    # The output files
+    my $output_dir  = catdir( 't', 'qr_codes' );
+    my $output_file = catfile( $output_dir, 'new_output.json' );
+
+    # Run the command line
+    system("$script -i $input_file -t $template_file -o $output_file");
+
+    # Compare the output_file and the reference_file
+    ok(
+        compare( $output_file, $reference_file ) == 0,
+        qq/Output matches the <$reference_file> file/
+    );
+    unlink($output_file);
+}
 
 
 sub compare_files {
--- utils/barcode/qr_code_utils.py
+++ utils/barcode/qr_code_utils.py
@@ -1,11 +1,14 @@
 import json
+import csv
 import os
 import argparse
 import re
-from PIL import Image
-from pyzbar.pyzbar import decode
+import zlib
+import base64
 import qrcode
 import glob
+from PIL import Image
+from pyzbar.pyzbar import decode
 
 # Utility functions
 def load_json_file(file_path):
@@ -14,38 +17,121 @@ def load_json_file(file_path):
         return json.load(file)
 
 def save_json_file(data, file_path):
-    """ Saves JSON data to a file. """
-    with open(file_path, 'w') as file:
+    """ Saves JSON data to a file and ensures it ends with a newline character. """
+    with open(file_path, 'w', newline='\n') as file:
         json.dump(data, file, indent=4)
+        file.write('\n')  # Add a newline character at the end
 
 # Functions for QR code generation from JSON
-def generate_qr_from_data(binary_digit_string, output_path):
+def compress_binary_string(binary_digit_string, compress=True):
+    if compress:
+        # Substitute '0' with '2' and '1' with '3' to avoid 
+        # byte_code.encode padding issues with leading zeroes
+        substituted_string = binary_digit_string.replace('0', '2').replace('1', '3')
+
+        # Convert to bytes and compress
+        byte_code = substituted_string.encode()  # Directly encode the ASCII string to bytes
+        compressed_bytes = zlib.compress(byte_code)
+        return compressed_bytes
+    else:
+        return binary_digit_string.encode()
+
+def decompress_binary_string(compressed_bytes):
+    try:
+        # Decompress the bytes
+        decompressed_bytes = zlib.decompress(compressed_bytes)
+
+        # Convert the decompressed bytes back to the substituted string
+        substituted_string = decompressed_bytes.decode()
+
+        # Revert the substitution
+        original_binary_string = substituted_string.replace('2', '0').replace('3', '1')
+        return original_binary_string
+    except Exception as e:
+        print("Error in decompression:", e)
+        return ''
+
+
+def generate_qr_from_data(binary_digit_string, output_path, compress=True):
+    if compress:
+        # Compress and then base64 encode the data
+        compressed_data = compress_binary_string(binary_digit_string, compress)
+        data_to_encode = base64.b64encode(compressed_data)
+    else:
+        # If not compressing, use the binary digit string directly
+        data_to_encode = binary_digit_string
+
+    # Create QR code
+    qr_version = 1  # Adjust based on data length
     qr = qrcode.QRCode(
-        version=1,
+        version=qr_version,
         error_correction=qrcode.constants.ERROR_CORRECT_L,
         box_size=10,
         border=4,
     )
-    qr.add_data(binary_digit_string)
+    qr.add_data(data_to_encode)
     qr.make(fit=True)
     img = qr.make_image(fill_color="black", back_color="white")
     img.save(output_path)
 
-def generate_qr_codes_from_json(json_data, output_dir):
+def generate_qr_codes_from_json(json_data, output_dir, compress=True):
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
-
     for key, value in json_data.items():
         if 'binary_digit_string' in value:
             binary_digit_string = value['binary_digit_string']
             safe_key = key.replace(':', '_')
             img_path = os.path.join(output_dir, f"{safe_key}.png")
-            generate_qr_from_data(binary_digit_string, img_path)
+            generate_qr_from_data(binary_digit_string, img_path, compress)
 
 # Functions for decoding QR codes to JSON
 def decode_qr_code(filepath):
+    # Open the image and decode the QR code
     data = decode(Image.open(filepath))
-    return data[0].data.decode() if data else None
+    if not data:
+        print(f"No QR code data found in file: {filepath}")
+        return None, None, False
+
+    qr_data = data[0].data
+
+    try:
+        # Try to base64 decode the data (assuming it might be compressed)
+        try:
+            base64_decoded_data = base64.b64decode(qr_data)
+        except base64.binascii.Error:
+            # If base64 decoding fails, assume data is not base64 encoded (hence, uncompressed)
+            base64_decoded_data = qr_data
+
+        # Try to decompress the data
+        try:
+            decompressed_data = zlib.decompress(base64_decoded_data)
+            # If decompression succeeds, process it further
+            decompressed_and_reverted_string = decompressed_data.decode().replace('2', '0').replace('3', '1')
+            is_compressed = True
+        except zlib.error:
+            # If decompression fails, assume the data is uncompressed
+            decompressed_and_reverted_string = base64_decoded_data.decode('utf-8', errors='ignore')
+            is_compressed = False
+
+        # Print the final decompressed and reverted string for verification
+        #print("Decompressed and reverted string:", decompressed_and_reverted_string)  # Diagnostic print
+
+        return qr_data, decompressed_and_reverted_string, is_compressed
+    except Exception as e:
+        print(f"Error during decoding: {e}")
+        return None, None, False
+
+    return qr_data, decompressed_and_reverted_string, is_compressed
+
+def is_data_compressed(qr_data):
+    # Check the first character to determine if data is compressed
+    try:
+        marker = qr_data[:1].decode()
+        return marker == 'C'
+    except Exception as e:
+        print(f"Error determining data compression: {e}")
+        return False
+
 
 def decode_binary_string(binary_string, json_template):
     keys = list(json_template.keys())
@@ -73,7 +159,6 @@ def unflatten(dictionary):
 def convert_curie_objects_to_array(data):
     def is_curie(key):
         return re.match(r'^\w+:[^:]+$', key)
-
     def recursive_convert(obj):
         if isinstance(obj, dict):
             curie_keys = [key for key in obj if is_curie(key) and isinstance(obj[key], dict)]
@@ -90,28 +175,46 @@ def convert_curie_objects_to_array(data):
                 for key in obj:
                     obj[key] = recursive_convert(obj[key])
         return obj
-
     return recursive_convert(data)
 
 def decode_qr_codes_to_json(file_paths, json_template):
     results = []
+
     for filepath in file_paths:
         if filepath.endswith('.png'):
-            binary_string = decode_qr_code(filepath)
-            if binary_string:
-                result = decode_binary_string(binary_string, json_template)
+            qr_data, binary_digit_string, is_compressed = decode_qr_code(filepath)
+
+            if binary_digit_string:
+                result = decode_binary_string(binary_digit_string, json_template)
                 unflattened_result = unflatten(result)
                 final_result = convert_curie_objects_to_array(unflattened_result)
                 results.append(final_result)
             else:
-                print(f"No QR code found in {filepath}")
+                print(f"No valid QR code data found in {filepath}")
+
     return results
+
+def generate_csv_from_pngs(png_folder, csv_file_path):
+    with open(csv_file_path, 'w', newline='\n') as csvfile:
+        csvwriter = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL, escapechar='\\')
+        csvwriter.writerow(["Name_of_PNG", "Raw_QR_Data", "Processed_Data", "Was_Compressed"])
+
+        for png_file in glob.glob(os.path.join(png_folder, '*.png')):
+            qr_data, processed_data, was_compressed = decode_qr_code(png_file)
+
+            if qr_data is not None:
+                # Convert raw QR data to a string format for CSV
+                qr_raw_str = qr_data.decode('utf-8', errors='ignore') if isinstance(qr_data, bytes) else str(qr_data)
+                csvwriter.writerow([os.path.basename(png_file), qr_raw_str, processed_data, was_compressed])
+            else:
+                print(f"Failed to decode QR code in file: {png_file}")
 
 # CLI setup functions
 def setup_qr_generation_cli():
     parser = argparse.ArgumentParser(description='Generate QR codes from JSON data.')
     parser.add_argument('-i', '--input', required=True, help='Input JSON file path')
     parser.add_argument('-o', '--output', default='qr_codes', help='Output directory for QR codes')
+    parser.add_argument('--no-compress', action='store_true', help='Disable compression of the binary digit string')
     return parser.parse_args()
 
 def setup_qr_decoding_cli():
@@ -119,13 +222,16 @@ def setup_qr_decoding_cli():
     parser.add_argument('-i', '--input', nargs='+', required=True, help='Input PNG files (e.g., "image1.png image2.png")')
     parser.add_argument('-t', '--template', required=True, help='JSON template file')
     parser.add_argument('-o', '--output', default='decoded.json', help='Output JSON file')
+    parser.add_argument('--generate-csv', action='store_true', help='Generate a CSV file with QR code data')
+    parser.add_argument('--csv-file', default='qr_data.csv', help='Output CSV file path (default: qr_data.csv)')
     return parser.parse_args()
 
 # Main functions for CLI
 def main_generate():
     args = setup_qr_generation_cli()
     json_data = load_json_file(args.input)
-    generate_qr_codes_from_json(json_data, args.output)
+    compress = not args.no_compress  # Determine whether to compress based on the --no-compress flag
+    generate_qr_codes_from_json(json_data, args.output, compress)
     print(f"QR codes generated successfully in {args.output}")
 
 def main_decode():
@@ -134,3 +240,6 @@ def main_decode():
     decoded_data = decode_qr_codes_to_json(args.input, json_template)
     save_json_file(decoded_data, args.output)
     print(f"Decoded data saved to {args.output}")
+    if args.generate_csv:
+        generate_csv_from_pngs(os.path.dirname(args.input[0]), args.csv_file)
+        print(f"CSV file generated at {args.csv_file}")
