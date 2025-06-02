5176abdda29355716d233f3ead0874ef6ad037c2
Manuel Rueda <rueda.manu@gmail.com>
2025-06-02 21:08:29 +0200

    Source commit

--- lib/Pheno/Ranker.pm
+++ lib/Pheno/Ranker.pm
@@ -28,7 +28,7 @@ $SIG{__DIE__}  = sub { die BOLD RED "Error: ", @_ };
 
 # Global variables:
 $Data::Dumper::Sortkeys = 1;
-our $VERSION   = '1.07_1';
+our $VERSION   = '1.07';
 our $share_dir = dist_dir('Pheno-Ranker');
 
 # Set development mode
